57ca6bc0eb687236403573513a8eb438d3d65b0a
jayminpadaria <jayminpadaria@gmail.com>
2024-08-08 19:00:34 +0530

    refactor: update db file

--- database/db.js
+++ database/db.js
@@ -1,9 +1,9 @@
 
-    user = [{"id":"debbac23-ad00-40a9-b095-014a5f710e53","name":"niraj","email":"test@emaial.com"}]
+    user = []
     
-    projects = [{"id":"d5fc95d0-2323-4f56-88d2-357c6ccbd045","name":"task menegement 2","description":"description 2","userId":"debbac23-ad00-40a9-b095-014a5f710e53"},{"id":"d9818c28-30af-4adb-b1e5-6f60c3e3a58f","name":"task menegement","description":"description","userId":"debbac23-ad00-40a9-b095-014a5f710e53"}];
+    projects = [];
     
-    task = [{"id":"d434379d-9901-4867-8cfa-b95e726fbb5a","title":"user crud API","description":"description","status":"Not started","projectId":"d5fc95d0-2323-4f56-88d2-357c6ccbd045","createdAt":"2024-08-08T12:54:28.078Z"},{"id":"8c798546-2e56-4a4e-a4c1-ba225a4763db","title":"task crud API","description":"description","status":"Not started","projectId":"d5fc95d0-2323-4f56-88d2-357c6ccbd045","createdAt":"2024-08-08T12:54:37.514Z"},{"id":"c38f351a-0099-49ab-89cb-427d70147625","title":"project crud API","description":"description","status":"Not started","projectId":"d5fc95d0-2323-4f56-88d2-357c6ccbd045","createdAt":"2024-08-08T12:54:47.097Z"}];
+    task = [];
     
     module.exports = { user, projects, task };
     
